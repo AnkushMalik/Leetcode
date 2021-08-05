15dff5a43afae8b147047485c28668f37ce2efbb
Ankush Malik <ankushmalik631@gmail.com>
2021-08-05 11:54:27 +0530

    better approach LCS

--- 1143.py
+++ 1143.py
@@ -40,3 +40,17 @@ class Solution:
             memo[i][j] =  max(same,diff1,diff2)
             return memo[i][j]
         return findLCS(text1,text2,0,0)
+
+#DP tabular better approach 91%:
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
+        
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i]==text2[j]:
+                    memo[i][j]=1+memo[i+1][j+1]
+                else:
+                    memo[i][j] = max(memo[i+1][j],memo[i][j+1])
+        return memo[0][0]
