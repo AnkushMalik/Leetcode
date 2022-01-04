0dcbd4329b0a167cd255f79c4d941832495884c6
Ankush Malik <ankushmalik631@gmail.com>
2022-01-04 15:33:50 +0530

    Time: 96 ms (33.29%), Space: 15.7 MB (15.81%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -11,19 +11,7 @@ class Solution:
             a = r1.val if r1 else 0
             b = r2.val if r2 else 0
             root = TreeNode(a+b)
-            if r1 and r2:
-                if r1.left and r2.left:
-                    root.left = dfs(r1 and r1.left, r2 and r2.left)
-                if r1.right and r2.right:
-                    root.right = dfs(r1 and r1.right, r2 and r2.right)
-
-                if not r1.left:
-                    root.left = r2.left if r2 else None
-                if not r1.right:
-                    root.right = r2.right if r2 else None
-                if not r2.left:
-                    root.left = r1.left if r1 else None
-                if not r2.right:
-                    root.right = r1.right if r1 else None
+            root.left = dfs(r1 and r1.left, r2 and r2.left)
+            root.right = dfs(r1 and r1.right, r2 and r2.right)
             return root
         return dfs(root1,root2)
