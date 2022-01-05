231831fc13946f8d318bf87e67e12171e3730f91
Ankush Malik <ankushmalik631@gmail.com>
2022-01-06 00:21:04 +0530

    Time: 76 ms (16.05%), Space: 14.3 MB (68.32%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,42 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        R = len(grid)
+        C = len(grid[0])
+        rotten = []
+        freshCount = 0
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j]==2:
+                    rotten.append([i,j])
+                elif grid[i][j]==1:
+                    freshCount+=1
+        count = 0
+        while rotten:
+            temp = []
+            for org in rotten:
+                i = org[0]
+                j = org[1]
+                if 0<i:
+                    if grid[i-1][j]==1:
+                        temp.append([i-1,j])
+                        grid[i-1][j]=2
+                        freshCount-=1
+                if i<R-1:
+                    if grid[i+1][j]==1:
+                        temp.append([i+1,j])
+                        grid[i+1][j]=2
+                        freshCount-=1
+                if 0<j:
+                    if grid[i][j-1]==1:
+                        temp.append([i,j-1])
+                        grid[i][j-1]=2
+                        freshCount-=1
+                if j<C-1:
+                    if grid[i][j+1]==1:
+                        temp.append([i,j+1])
+                        grid[i][j+1]=2
+                        freshCount-=1
+            rotten = temp
+            if rotten:
+                count+=1
+        return count if not freshCount else -1
