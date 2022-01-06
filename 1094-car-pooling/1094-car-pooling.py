4cc9094b1d7765b93f07d44a8647f9b617e5f5d0
Ankush Malik <ankushmalik631@gmail.com>
2022-01-06 22:33:34 +0530

    Time: 352 ms (5.55%), Space: 15 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,27 +1,23 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         hp = []
-        
-        for i in trips:
-            heappush(hp,(i[1],(i[0],i[2])))
+
+        for i in trips: heappush(hp,(i[1],(i[0],i[2])))
         
         incar = []
         passCount = 0
         while(hp):
             newP = heappop(hp)
             count = newP[1][0]
-            if passCount+count>capacity:
-                i = 0
-                while(i<len(incar)):
-                    pssg = incar[i]
-                    if pssg[1][1]<=newP[0]:
-                        passCount-=pssg[1][0]
-                        incar.pop(i)
-                        i-=1
-                    i+=1
+            i = 0
+            while(i<len(incar)):
+                pssg = incar[i]
+                if pssg[1][1]<=newP[0]:
+                    passCount-=pssg[1][0]
+                    incar.pop(i)
+                    i-=1
+                i+=1
             if passCount+count>capacity: return False
             incar.append(newP)
             passCount+=count
         return True
-                        
-        
