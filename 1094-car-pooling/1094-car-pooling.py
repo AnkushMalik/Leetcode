3e76eca0b9ea56515b8066eb4914db7911256e6a
Ankush Malik <ankushmalik631@gmail.com>
2022-01-06 22:46:57 +0530

    Time: 98 ms (22.26%), Space: 14.9 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -2,8 +2,8 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         ts = []
         for trip in trips:
-            ts.append([trip[1],trip[0]])
-            ts.append([trip[2],-trip[0]])
+            ts.append((trip[1],trip[0]))
+            ts.append((trip[2],-trip[0]))
         ts.sort()
         curr_pass = 0
         for time, passChange in ts:
