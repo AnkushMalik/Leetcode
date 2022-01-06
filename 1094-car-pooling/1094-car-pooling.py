e24265f2853a880f30863d95547c63c39e6e3445
Ankush Malik <ankushmalik631@gmail.com>
2022-01-06 22:46:28 +0530

    Time: 104 ms (20.17%), Space: 14.7 MB (77.09%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,19 +1,13 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        hp = sorted([[i[1],(i[0],i[2])] for i in trips])
-        incar = []
-        passCount = 0
-        for newP in hp:
-            count = newP[1][0]
-            i = 0
-            while(i<len(incar)):
-                pssg = incar[i]
-                if pssg[1][1]<=newP[0]:
-                    passCount-=pssg[1][0]
-                    incar.pop(i)
-                    i-=1
-                i+=1
-            if passCount+count>capacity: return False
-            incar.append(newP)
-            passCount+=count
+        ts = []
+        for trip in trips:
+            ts.append([trip[1],trip[0]])
+            ts.append([trip[2],-trip[0]])
+        ts.sort()
+        curr_pass = 0
+        for time, passChange in ts:
+            curr_pass += passChange
+            if curr_pass>capacity:
+                return False
         return True
