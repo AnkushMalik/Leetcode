6d5509b53549a7b05e5254ae7b9d66e2f5559f61
Ankush Malik <ankushmalik631@gmail.com>
2022-01-07 21:49:40 +0530

    Time: 464 ms (63.52%), Space: 15.7 MB (54.18%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,14 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         self.ans = []
-        self.findAllComb(list(range(1,n+1)),[],k)
+        self.findAllComb(1,n+1,[],k)
         return self.ans
 
-    def findAllComb(self, arr, carr, lim):
+    def findAllComb(self, start, end, carr, lim):
         if len(carr)==lim:
             self.ans.append(carr[:])
         else:
-            for i in range(len(arr)):
-                carr.append(arr[i])
-                self.findAllComb(arr[i+1:],carr,lim)
+            for i in range(start,end):
+                carr.append(i)
+                self.findAllComb(i+1, end, carr,lim)
                 carr.pop()
