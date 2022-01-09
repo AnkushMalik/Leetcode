054e71c4f1e2609330b6d6ed1950af9b5886f1ad
Ankush Malik <ankushmalik631@gmail.com>
2022-01-09 21:03:24 +0530

    Time: 84 ms (27.41%), Space: 17.4 MB (5.99%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -5,11 +5,8 @@ class Solution:
         return memo[(0,0)]
     
     def findMinPath(self,triangle,i,j,memo):
-        if i>=len(triangle): return 0
-        if j<0 or j>=len(triangle[i]): return 0
-        
+        if i>=len(triangle) or j<0 or j>=len(triangle[i]): return 0
         if (i,j) in memo: return memo[(i,j)]
-        
         memo[(i,j)] = triangle[i][j] + min(self.findMinPath(triangle,i+1,j,memo),self.findMinPath(triangle,i+1,j+1,memo))
         return memo[(i,j)]
         
