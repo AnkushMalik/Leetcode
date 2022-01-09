e975c037c1de5595d6d8c88d9534f5caeacd29fc
Ankush Malik <ankushmalik631@gmail.com>
2022-01-09 21:08:30 +0530

    Time: 106 ms (13.86%), Space: 17.4 MB (5.99%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         memo = {}
-        self.findMinPath(triangle,0,0,memo)
-        return memo[(0,0)]
+        def findMinPath(i,j):
+            if i>=len(triangle) or j<0 or j>=len(triangle[i]): return 0
+            if (i,j) in memo: return memo[(i,j)]
+            memo[(i,j)] = triangle[i][j] + min(findMinPath(i+1,j),findMinPath(i+1,j+1))
+            return memo[(i,j)]
+        
+        return findMinPath(0,0)
     
-    def findMinPath(self,triangle,i,j,memo):
-        if i>=len(triangle) or j<0 or j>=len(triangle[i]): return 0
-        if (i,j) in memo: return memo[(i,j)]
-        memo[(i,j)] = triangle[i][j] + min(self.findMinPath(triangle,i+1,j,memo),self.findMinPath(triangle,i+1,j+1,memo))
-        return memo[(i,j)]
+    
         
