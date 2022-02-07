37fb94daa6d8d1aa9bbebc06b1db29b3a69a07b6
Ankush Malik <ankushmalik631@gmail.com>
2022-02-07 22:45:51 +0530

    Time: 32 ms (89.14%), Space: 14 MB (79.84%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,12 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        sHash = self.genHash(s)
-        tHash = self.genHash(t)
-        for i in range(26):
-            if sHash[i]!=tHash[i]:
-                return chr(i+97)
-    def genHash(self,s):
-        arr = [0]*26
-        for i in s:
-            arr[ord(i) - ord('a')]+=1
-        return arr
+        s_ord, t_ord = 0, 0
+        for ss in s:
+         s_ord += ord(ss)
+        for tt in t:
+         t_ord += ord(tt)
+
+        return chr(t_ord - s_ord)
