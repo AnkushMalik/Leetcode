7f0a06f22c587daf5e0d5648a2db79db05445fd1
Ankush Malik <ankushmalik631@gmail.com>
2022-03-01 15:25:12 +0530

    Time: 130 ms (39.20%), Space: 18 MB (67.51%) - LeetHub

--- 1490-clone-n-ary-tree/1490-clone-n-ary-tree.py
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.py
@@ -3,7 +3,6 @@ class Solution:
         if root: ans = Node(root.val,[])
         else: return None
         def dfs(node,orig):
-            if not orig: return None
             for i in orig.children:
                 node.children.append(Node(i.val,[]))
                 dfs(node.children[-1],i)
