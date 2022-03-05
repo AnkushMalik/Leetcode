4c6c3652becdfe45794e2f3070e72a0d0be58772
Ankush Malik <ankushmalik631@gmail.com>
2022-03-05 14:15:44 +0530

    Time: 36 ms (89.86%), Space: 14.1 MB (50.06%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -5,11 +5,12 @@ class Solution:
         def checkOrder(s1,s2):
             n = min(len(s1),len(s2))
             for i in range(n):
-                if order.index(s1[i])<order.index(s2[i]): return True
-                elif order.index(s1[i])>order.index(s2[i]): return False
+                p = order.index(s1[i])
+                q = order.index(s2[i])
+                if p<q: return True
+                elif p>q: return False
             if s1[:n]==s2[:n]:
                 return False if len(s1)>len(s2) else True
-            return False
         
         for i in range(1,len(words)):
             if not checkOrder(words[i-1],words[i]):
