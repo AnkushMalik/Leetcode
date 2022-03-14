3a8368d4c7798c2ee6357fa15d5f2057f7900cf8
Ankush Malik <ankushmalik631@gmail.com>
2022-03-14 10:08:07 +0530

    Time: 39 ms (71.80%), Space: 14 MB (57.22%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,6 +1,5 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        ans = ''
         stk = []
         temp = path.split('/')
         for i in temp:
@@ -9,6 +8,4 @@ class Solution:
                     if stk: stk.pop()
                 elif i!='.':
                     stk.append(i)
-        for i in stk:
-            ans+='/'+i
-        return ans if len(ans) else '/'
+        return '/'+'/'.join(stk)
