46c57812d3e9178696ca24789306e74c7536cb51
Ankush Malik <ankushmalik631@gmail.com>
2022-03-15 14:55:59 +0530

    Time: 1172 ms (10.79%), Space: 19.3 MB (5.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -10,7 +10,7 @@ class Solution:
                     if stk and s[stk[-1]]=='(': stk.pop()
                     else:
                         stk.append(i)
-        for i in range(len(s)):
-            if i not in stk:
-                ans+=s[i]
+        indices = [e for e in range(len(s)) if e not in stk]
+        for i in indices:
+            ans+=s[i]
         return ans
