9e32973c7991f72d52e9a60660dc66d75047b0e0
Ankush Malik <ankushmalik631@gmail.com>
2022-04-17 03:06:49 +0530

    Time: 82 ms (89.55%), Space: 16.7 MB (53.53%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -5,18 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def convertBST(self, root: Optional[TreeNode], carry=0) -> Optional[TreeNode]:
-        if not root: return root
-        if not root.right:
-            root.val+=carry
-        if root.right:
-            self.convertBST(root.right, carry)
-            rtSmallest = self.findSmallest(root.right)
-            root.val += rtSmallest.val
-        if root.left: self.convertBST(root.left, root.val)
+    total = 0
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            self.convertBST(root.right)
+            self.total+=root.val
+            root.val = self.total
+            self.convertBST(root.left)
         return root
-    
-    def findSmallest(self, node):
-        if node.left:
-            return self.findSmallest(node.left)
-        return node
