4dfe24a01364491a13dc4d8285a7d9c45c93d13f
Ankush Malik <ankushmalik631@gmail.com>
2022-05-06 06:39:45 +0530

    Time: 1118 ms (5.01%), Space: 14.9 MB (8.08%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,27 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        h1, h2 = [0]*58, [0]*58        
+        h1, h2 = {},{}
         for i in t:
             if i not in s: return ''
-            h2[ord(i)-ord('A')]+=1
+            if i in h2: h2[i]+=1
+            else: h2[i]=1
 
         def isValid(h):
-            for i in range(58):
-                if h2[i] and h2[i]>h[i]: return False
+            for i in h2:
+                if i not in h or h2[i]>h[i]: return False
             return True
         
         ans = s+t
         stk = []
         for i in range(len(s)):
-            idx = ord(s[i])-ord('A')
-            h1[idx]+=1
+            if s[i] in h1: h1[s[i]]+=1
+            else: h1[s[i]]=1
+
             stk.append(s[i])
             check = isValid(h1)
+
             while(check):
                 if len(stk)<len(ans): ans = ''.join(stk)
                 elem = stk.pop(0)
-                eid = ord(elem) - ord('A')
-                h1[eid] -= 1
+                h1[elem] -= 1
                 check = isValid(h1)
         return ans if len(ans)<=len(s) else ''
                 
