e69edbb12f9a9cd730a97056fcf9915a9ee59d71
Ankush Malik <ankushmalik631@gmail.com>
2022-05-11 03:05:06 +0530

    Time: 52 ms (31.19%), Space: 13.9 MB (29.59%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -5,11 +5,11 @@ class Solution:
         return self.ans
     
     def findAllCombination(self, path, k, n):
-        if len(path)==k:
-            if sum(path)==n: self.ans.append(path)
+        if k==0 and sum(path)==n: self.ans.append(path)
         else:
             start = path[-1]+1 if path else 1
+            pathSum = sum(path)
             for i in range(start,10):
-                if sum(path)+i <= n:
-                    self.findAllCombination(path+[i], k, n)
+                if pathSum+i <= n:
+                    self.findAllCombination(path+[i], k-1, n)
             
