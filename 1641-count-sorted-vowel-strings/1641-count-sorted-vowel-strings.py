ab9d99a5b8c85157459db3d8a227a4f8cfdda922
Ankush Malik <ankushmalik631@gmail.com>
2022-05-11 08:21:56 +0530

    Time: 6356 ms (6.60%), Space: 13.9 MB (27.45%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,14 +1,12 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        vowel = ['a', 'e', 'i', 'o', 'u']
-        count = 0
         def findCombs(vowel, n):
-            if n==1: return len(vowel)
+            if n==1: return vowel
             k = 0
-            for i in range(len(vowel)):
-                k+= findCombs(vowel[i:], n-1)
+            for i in range(vowel):
+                k+= findCombs(vowel-i, n-1)
             return k
-        return findCombs(vowel, n)
+        return findCombs(5, n)
                 
                 
             
